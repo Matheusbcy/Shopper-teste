98bd15dcccf4be3cbe269023f04b04b4f69020d5
Matheusbcy <matheusbcy@gmail.com>
2023-05-16 15:00:25 -0300

    excluindo getNewProd

--- front-end-react/src/utils/GetNewProd.js
+++
@@ -1,6 +0,0 @@
- export function getNewPrice(listProd, code) {
-    let arrNewPriceFiltered = listProd.filter((d) =>
-      d.product_code.includes(code)
-    );
-    return arrNewPriceFiltered[0].new_price;
-  }
